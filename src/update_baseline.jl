af2abb8aa49b07576dd0012c93a1eb262106d5bc
coffm049 <coffm049@umn.edu>
2024-12-11 20:35:26 -0600

    Figured out how to keep mu0 and sigma0 fixed at 0

--- src/update_baseline.jl
+++ src/update_baseline.jl
@@ -15,7 +15,8 @@ function update_mu0!(base::Union{Baseline_NormDLUnif, Baseline_NormUnif},
     v1 = 1.0 / (1.0 / prior_mu0_sig2 + n / sig02_now)
     m1 = v1 * (prior.mu0_mean / prior_mu0_sig2 + sum_mu / sig02_now )
 
-    base.mu0 = randn()*sqrt(v1) + m1
+    #base.mu0 = randn()*sqrt(v1) + m1
+    base.mu0 = 0.0 
     return nothing
 end
 
@@ -39,14 +40,16 @@ function update_sig0!(base::Union{Baseline_NormDLUnif, Baseline_NormUnif},
     sigout, lt, it = shrinkSlice(base.sig0, 0.0, prior.sig0_upper,
                     logtarget, TargetArgs_NormSigUnif(mu_vec, base.mu0),
                     sliceiter)
-    base.sig0 = sigout
+    #base.sig0 = sigout
+    base.sig0 = 0.0
     return nothing
 end
 
 function update_baseline!(model::Model_PPMx, update_params::Vector{Symbol}, sliceiter::Int)
 
     K = length(model.state.lik_params)
-    mu_vec = [ model.state.lik_params[k].mu for k in 1:K ]
+    #mu_vec = [ model.state.lik_params[k].mu for k in 1:K ]
+    mu_vec = [ 0.0 for k in 1:K ]
 
     if :mu0 in update_params
         update_mu0!(model.state.baseline, mu_vec, model.prior.baseline)
