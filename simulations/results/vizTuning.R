1ddd0ea8de29ba4b2f11cbbe4d9ad247f2485f2a
coffm049 <coffm049@umn.edu>
2025-05-10 19:34:20 -0500

    Updating sim tuning vizualizations

--- simulations/results/vizTuning.R
+++ simulations/results/vizTuning.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(patchwork)
 
 df <- data.frame("file" = dir()) %>%
     filter(grepl("prec", file)) %>%
@@ -24,3 +25,56 @@ df %>%
     geom_density(alpha = 0.3) +
     geom_vline(aes(xintercept = 3))
 # facet
+
+temp <- df %>%
+    reframe(
+        # type 2 error
+        mixT2 = mean(zeroInDPM),
+        kT2 = mean(kclustIn0),
+
+        # coverage
+        mixCov = mean(commonInDPM),
+        kCov = mean(kclustInCommon),
+        .by = c(alph, bet, prec)
+    ) %>%
+    pivot_longer(cols = c(mixT2:kCov)) %>%
+    mutate(
+        stat = ifelse(grepl("T2", name), "T2", "Coverage"),
+        model = ifelse(grepl("k", name), "kclust", "mixDPM"),
+    )
+p1 <- temp %>%
+    filter(stat == "T2") %>%
+    ggplot(aes(x = alph, y = bet, fill = value)) +
+    geom_tile() +
+    scale_fill_gradient2(
+        low = "forestgreen", mid = "white", high = "red",
+        midpoint = 0.75
+    ) +
+    facet_wrap(~prec) +
+    ggtitle("Type 2 error")
+p2 <- temp %>%
+    filter(stat == "Coverage") %>%
+    ggplot(aes(x = alph, y = bet, fill = value)) +
+    geom_tile() +
+    scale_fill_gradient2(
+        low = "red", mid = "white", high = "forestgreen",
+        midpoint = 0.75
+    ) +
+    facet_wrap(~prec) +
+    ggtitle("Coverage")
+
+# mse
+temp2 <- df %>%
+    reframe(mseMix = sqrt(mean((common - meanBeta1)^2)), mseK = sqrt(mean((common - meanBetakclust1)^2)), .by = c(alph, bet, prec))
+
+p3 <- temp2 %>%
+    ggplot(aes(x = alph, y = bet, fill = mseMix)) +
+    geom_tile() +
+    scale_fill_gradient2(
+        low = "forestgreen", mid = "white", high = "red",
+        midpoint = 2.4
+    ) +
+    facet_wrap(~prec) +
+    ggtitle("MSE")
+
+p1 / p2 / p3
