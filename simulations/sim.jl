c9f7d3c10817d12d50e58245f329c6fc00b44510
coffm049 <coffm049@umn.edu>
2025-05-18 03:48:10 -0500

    Confirmed no missing in results output

--- simulations/sim.jl
+++ simulations/sim.jl
@@ -8,7 +8,7 @@ using LinearAlgebra
 using HypothesisTests
 using LaTeXStrings
 using SpecialFunctions
-using SummaryTables 
+using SummaryTables
 using StatsPlots
 using Clustering
 using DataFrames
@@ -21,15 +21,15 @@ include("simFunctions.jl")
 # read arguments from command line
 # N, nc, variance, interEffect, common, xdiff
 N = parse(Int, ARGS[1])
-nc =parse(Int, ARGS[2])
+nc = parse(Int, ARGS[2])
 variance = parse(Float64, ARGS[3])
 interEffect = parse(Float64, ARGS[4])
 common = parse(Float64, ARGS[5])
-xdiff=parse(Float64, ARGS[6])
-dims=parse(Int, ARGS[7])
-prec=parse(Float64, ARGS[8])
-alph=parse(Float64, ARGS[9])
-bet=parse(Float64, ARGS[10])
+xdiff = parse(Float64, ARGS[6])
+dims = parse(Int, ARGS[7])
+prec = parse(Float64, ARGS[8])
+alph = parse(Float64, ARGS[9])
+bet = parse(Float64, ARGS[10])
 
 # construct a file name from the user inputs
 outputName = "results/c$(nc)_inter$(interEffect)_common$(common)_xd$(xdiff)_v$(variance)_dim$(dims)_prec$(prec)alph$(alph)bet$(bet)"
@@ -37,8 +37,8 @@ outputName = "results/c$(nc)_inter$(interEffect)_common$(common)_xd$(xdiff)_v$(v
 # END user input
 
 reps = 100
-niters=5000
-fractions = repeat([1/nc], nc)
+niters = 5000
+fractions = repeat([1 / nc], nc)
 
 # END other controls
 
@@ -48,17 +48,17 @@ seeds = MersenneTwister.(rand(1:10^8, Threads.nthreads()))  # or generate from o
 # n,  fractions, variance, interEffect, common
 Threads.@threads for i in 1:reps
     try
-       println(i)
-       results[i] = simExperiment(seeds[Threads.threadid()]; N=N, fractions=fractions, variance = variance, interEffect = interEffect, common = common , niters=niters,  plotSim =  false, xdiff = xdiff, dims = dims, prec = prec, alph = alph, bet = bet)
+        println(i)
+        results[i] = simExperiment(seeds[Threads.threadid()]; N=N, fractions=fractions, variance=variance, interEffect=interEffect, common=common, niters=niters, plotSim=false, xdiff=xdiff, dims=dims, prec=prec, alph=alph, bet=bet)
     catch err
         println("sim Failed")
     end
-end   
+end
 defined_results = [results[i] for i in 1:reps if isassigned(results, i)]
 df = vcat(defined_results...)
 
 if !isfile("$(outputName).csv")
-    CSV.write("$(outputName).csv", df, writeheader = true, append = false)
-else 
-    CSV.write("$(outputName).csv", df, writeheader = false, append = true)
+    CSV.write("$(outputName).csv", df, writeheader=true, append=false)
+else
+    CSV.write("$(outputName).csv", df, writeheader=false, append=true)
 end
